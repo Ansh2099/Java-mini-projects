57a614be0a6b88d72c20fc715852e5ef5fa26b11
Ansh2099 <anshpreetsingh1009@gmail.com>
2024-08-12 13:16:56 +0530

    A Basic program implementing a math equation

---
+++ MathMagic.java
@@ -0,0 +1,20 @@
+public class MathMagic{
+
+//this program uses a equation which will return 3, no matter the input no.
+
+    public static void main(String[] args){
+        
+        int myNumber = 5677;
+
+     // Here myNumber will be considered the original no. from now on
+
+        int stepOne = myNumber*myNumber;
+        int stepTwo = stepOne + myNumber;
+        int stepThree = stepTwo/ myNumber;
+        int stepFour = stepThree + 17;
+        int stepFive = stepFour - myNumber;
+        int stepSix = stepFive/6;
+
+        System.out.println(stepSix);
+    }
+}
