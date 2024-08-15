b675d86c15e3e7cba3c101fa8dd5aac7f1033543
Ansh2099 <anshpreetsingh1009@gmail.com>
2024-08-15 22:55:46 +0530

    Basic program ArrayList practise

---
+++ PlantingaTree.java
@@ -0,0 +1,20 @@
+
+
+public class PlantingaTree {
+
+    public static void main(String[] args){
+
+    /* This project will help me introduce myself and plant a tree in user's screen **/
+        
+        System.out.println("Hey There! I am Anshpreet and I am learning to code in Java!!!");
+        System.out.println("I am going to plant a tree today!!");
+        System.out.println("   ***   ");
+        System.out.println("  *****  ");
+        System.out.println(" ******* ");
+        System.out.println("   ***   ");
+        System.out.println("    *    ");
+        System.out.println("    *    ");
+        
+    }
+
+}
